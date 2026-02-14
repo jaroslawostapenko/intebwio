fcb6da66f8c7903963a38b3a7f500530598bc0a4
jaroslawostapenko <hjkhhhhgvbjjgcfg@gmail.com>
2026-02-14 11:08:49 +0100

    Change base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './jaroslawostapenko/intebwio', // Ensures assets are loaded correctly on GitHub Pages (subdirectory)
+  base: '/', // Ensures assets are loaded correctly on GitHub Pages (subdirectory)
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
